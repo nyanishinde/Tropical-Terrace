9aa9b664e50997e33eb17956eb3398096dbe6143
nyanishinde <nyanishinde10@gmail.com>
2024-08-01 18:52:01 +0530

    Completed backend for get started activity

--- app/src/main/java/com/example/tropicalterrace/GetStarted.kt
+++ app/src/main/java/com/example/tropicalterrace/GetStarted.kt
@@ -1,14 +1,26 @@
 package com.example.tropicalterrace
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tropicalterrace.databinding.ActivityGetStartedBinding
 
 class GetStarted : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGetStartedBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityGetStartedBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_get_started)
+
+        binding.btnGetStarted.setOnClickListener {
+            startActivity(Intent(this,Signin::class.java))
+            finish()
+        }
+
     }
 }
