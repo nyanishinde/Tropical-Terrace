f4cd17ca4ba2c8361f920659347595a1dd1865c0
nyanishinde <nyanishinde10@gmail.com>
2024-08-04 07:40:10 +0530

    Completed backend for get started activity

--- app/src/main/java/com/example/tropicalterrace/Signup.kt
+++ app/src/main/java/com/example/tropicalterrace/Signup.kt
@@ -5,17 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.tropicalterrace.databinding.ActivitySigninBinding
 
-private lateinit var binding: ActivitySigninBinding
-
-class Signin : AppCompatActivity() {
+class Signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivitySigninBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_signin)
-
-        binding
-
+        setContentView(R.layout.activity_signup)
     }
 }
