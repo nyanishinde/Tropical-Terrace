c7477bfa0718903dc357a4b488d7b8fddc6cf23f
nyanishinde <nyanishinde10@gmail.com>
2024-08-04 09:58:05 +0530

    designed activity_signup.xml

--- app/src/main/java/com/example/tropicalterrace/Signin.kt
+++ app/src/main/java/com/example/tropicalterrace/Signin.kt
@@ -1,13 +1,16 @@
 package com.example.tropicalterrace
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tropicalterrace.databinding.ActivitySigninBinding
 
-private lateinit var binding: ActivitySigninBinding
+    private lateinit var binding: ActivitySigninBinding
 
 class Signin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +18,9 @@ class Signin : AppCompatActivity() {
         binding=ActivitySigninBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_signin)
 
-        binding
-
+        val signupBtn = findViewById<TextView>(R.id.signupBtn)
+        signupBtn.setOnClickListener {
+            startActivity(Intent(this,Signup::class.java))
+        }
     }
 }
