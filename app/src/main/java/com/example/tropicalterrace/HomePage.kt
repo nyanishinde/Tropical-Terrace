9be8fea251345bc40b424060b61a72c977d25e01
nyanishinde <nyanishinde10@gmail.com>
2024-08-08 21:59:26 +0530

    Created the HomePage

--- app/src/main/java/com/example/tropicalterrace/HomePage.kt
+++ app/src/main/java/com/example/tropicalterrace/HomePage.kt
@@ -9,7 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_home_page)
 
     }
