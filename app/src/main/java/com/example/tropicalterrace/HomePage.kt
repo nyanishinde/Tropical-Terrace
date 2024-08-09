872b730c044a5ce4762d5869097789751ffc16c0
nyanishinde <nyanishinde10@gmail.com>
2024-08-09 21:10:53 +0530

    Created all the fragments for bottom navigation

--- app/src/main/java/com/example/tropicalterrace/HomePage.kt
+++ app/src/main/java/com/example/tropicalterrace/HomePage.kt
@@ -5,11 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class HomePage : AppCompatActivity() {
+    class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+        val navbar = findViewById<BottomNavigationView>(R.id.navbarView)
+
+        navbar.setOnNavigationItemSelectedListener { menuitem ->
+            
+        }
 
     }
 }
